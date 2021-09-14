7394ade3c150251f00c08e524ffb41553d42baf9
daviddpi <piscopodavid97@gmail.com>
2021-09-14 17:09:36 +0200

    sistemata funzione che cambia il colore del box

--- js/myscript.js
+++ js/myscript.js
@@ -6,12 +6,20 @@ let main = new Vue({
         miaImmagine: "https://www.wallpapertip.com/wmimgs/83-838195_javascript-javascript-hd.png",
         myClassJs: "text-success fw-bold pt-5 text-center",
         testoP: "Sono un paragrafo scritto con js e anche la classe e inserita tramite js",
-        myBoxClass: ""
+        myBoxClass: "bg-red",
     },
     methods:{
-        changeColorBox: () =>{
-            this.myBoxClass = "mb-5 bg-red"
-            console.log(myBoxClass);
+        changeColorBox(){
+            if(this.myBoxClass == "bg-red"){
+                this.myBoxClass = "bg-blu";
+            } else if(this.myBoxClass == "bg-blu"){
+                this.myBoxClass = "bg-yellow";
+            } else if(this.myBoxClass == "bg-yellow"){
+                this.myBoxClass = "bg-white";
+            } else if(this.myBoxClass == "bg-white"){
+                this.myBoxClass = "bg-red";
+            }
+            console.log(this.myBoxClass);
         }
     }
 });
