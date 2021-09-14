dc975cb523a1274489b6af3319f64269d6ee7431
daviddpi <piscopodavid97@gmail.com>
2021-09-14 16:47:55 +0200

    commit esercizio vuejs

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+let main = new Vue({
+
+    el: "#root",
+    data: {
+        titolo: "Ciao sono un titolo scritto su js",
+        miaImmagine: "https://www.wallpapertip.com/wmimgs/83-838195_javascript-javascript-hd.png",
+        myClassJs: "text-success fw-bold pt-5 text-center",
+        testoP: "Sono un paragrafo scritto con js e anche la classe e inserita tramite js",
+        myBoxClass: ""
+    },
+    method:{
+        changeColorBox: () =>{
+            this.myBoxClass = "bg-red"
+        }
+    }
+});
